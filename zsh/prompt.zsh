b180a8609b8c085990072ef172c65a16ae2e1dba
Mike Kruk <tamagokun@gmail.com>
2012-07-27 15:48:23 -0400

    change git to /usr/local/bin/git since for some reason mountain lion no longer has /usr/bin/git

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,11 +1,11 @@
 autoload colors && colors
 
 git_branch() {
-  echo $(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+  echo $(/usr/local/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
 }
 
 git_dirty() {
-  st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
+  st=$(/usr/local/bin/git status 2>/dev/null | tail -n 1)
   if [[ $st == "" ]]
   then
 	echo ""
@@ -20,13 +20,13 @@ git_dirty() {
 }
 
 git_prompt_info() {
- ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
+ ref=$(/usr/local/bin/git symbolic-ref HEAD 2>/dev/null) || return
 # echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
  echo "${ref#refs/heads/}"
 }
 
 unpushed() {
-  /usr/bin/git cherry -v @{upstream} 2>/dev/null
+  /usr/local/bin/git cherry -v @{upstream} 2>/dev/null
 }
 
 need_push() {
