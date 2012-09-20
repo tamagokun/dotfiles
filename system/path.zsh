3d8b6fc10c4e68ec1334039cd7cad8b1675b2ac2
Mike Kruk <tamagokun@gmail.com>
2012-09-19 21:47:34 -0400

    added phpenv info to PATH

--- system/path.zsh
+++ system/path.zsh
@@ -2,4 +2,7 @@ export PATH="./bin:$ZSH/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/b
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
 
 #rbenv needs to be first
-export PATH="$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+export PATH="$HOME/.rbenv/shims:$HOME/.rbenv/bin:$HOME/.phpenv/bin:$PATH"
+
+#phpenv
+eval "$(phpenv init -)"
