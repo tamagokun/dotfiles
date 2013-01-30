c2891a02a70c8b829c5b5c6cdb125a0dd47a4f96
AUTHORNAME <AUTHOREMAIL>
2013-01-30 10:49:02 -0500

    cleaned up tmux powerline

--- bin/tmux-right-status.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-battery=`~/dotfiles/tmux/segments/battery_mac.sh`
-weather=`~/dotfiles/tmux/segments/weather.sh`
-
-
-echo -n "#[fg=colour137]⮂#[fg=colour127,bg=colour137,nobold] $battery #[fg=colour37]⮂#[fg=colour255,bg=colour37,nobold] $weather ⮂#[fg=colour16,bg=colour254,bold] #h "
