34c8aa02e92b6a3e3c967ed30267186bacc51f2e
Mike Kruk <tamagokun@gmail.com>
2013-02-22 08:56:25 -0500

    using new python powerline. remove default editor in git config. Lets rely on EDITOR

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:$HOME/.rbenv/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="./bin:$HOME/.rbenv/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/usr/local/share/python:$PATH"
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
