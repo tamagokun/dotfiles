4467f4e16bbd1a86239d52b2faa538a6149e2232
Mike Kruk <mike@ripeworks.com>
2013-10-03 09:20:18 -0400

    a few fixes for ubuntu

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,7 @@
 # GRC colorizes nifty unix tools all over the place
 if $(grc &>/dev/null)
 then
-  source `brew --prefix`/etc/grc.bashrc
+	if [ `uname` = "Darwin" ]; then
+		source `brew --prefix`/etc/grc.bashrc
+	fi
 fi
