60aef43b1549231e7f6117be754487c8b70791fd
Mike Kruk <tamagokun@gmail.com>
2013-10-07 19:55:50 -0400

    blah

--- install.sh
+++ install.sh
@@ -37,9 +37,9 @@ if ! [ -f git/gitconfig.symlink ]; then
 	notify "Configuring your .gitconfig"
 	git_credential='osxkeychain'
 
-	user ' - What is your github author name?'
+	info "What is your git author name?"
 	read -e git_authorname
-	user ' - What is your github author email?'
+	info "What is your git author email?"
 	read -e git_authoremail
 
 	sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" git/gitconfig.symlink.example > git/gitconfig.symlink
