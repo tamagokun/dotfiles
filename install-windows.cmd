5c28d2a83a8b6fd3643c14910249e9a692f989f1
Mike Kruk <tamagokun@gmail.com>
2014-10-22 14:35:45 -0400

    Delete install-windows.cmd

--- install-windows.cmd
+++
@@ -1,8 +0,0 @@
-@if not exist "%HOME%" @set HOME=%USERPROFILE%
-
-@set BASE_DIR=%HOME%\dotfiles
-call mklink /J "%HOME%\.vim" "%BASE_DIR%\vim\vim.symlink"
-call mklink "%HOME%\.vimrc" "%BASE_DIR%\vim\vimrc.symlink"
-call mklink "%HOME%\_vimrc" "%BASE_DIR%\vim\vimrc.symlink"
-call mklink "%HOME%\.gvimrc" "%BASE_DIR%\vim\gvimrc.symlink"
-call mklink "%HOME%\_gvimrc" "%BASE_DIR%\vim\gvimrc.symlink"
