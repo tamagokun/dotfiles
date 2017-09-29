afaf4428ba890f8231e3d9687f9889d5942e3281
Mike Kruk <tamagokun@gmail.com>
2017-09-29 13:59:14 -0400

    update hyperterm config for 2.0+

--- hyperterm.js
+++ hyperterm.js
@@ -1,5 +1,7 @@
 module.exports = {
   config: {
+    updateChannel: 'canary',
+
     // default font size for all tabs
     fontSize: 18,
 
@@ -7,41 +9,48 @@ module.exports = {
     fontFamily: 'OperatorMono-Book, Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
     // terminal cursor background color
-    cursorColor: 'rgba(32,187,252,0.7)',
+    // cursorColor: 'rgba(32,187,252,0.7)',
+
+    cursorShape: 'UNDERLINE',
 
     // terminal background color
     backgroundColor: 'rgba(4, 9, 15, 0.8)',
 
-    padding: '0 0',
+    // show menu on Linux
+    showHamburgerMenu: true,
+
+    // padding: '0 0',
 
     windowSize: [1200, 800],
+
+    css: '',
+
+    termCSS: '',
 
     // some color overrides. see http://bit.ly/29k1iU2 for
     // the full list
     // Tomorrow Night 80s
-    colors: [
-      '#000000',
-      '#f78d8c',
-      '#a8d4a9',
-      '#ffd479',
-      '#78aad6',
-      '#d7acd6',
-      '#76d4d6',
-      '#ffffff',
-      '#000000',
-      '#f78d8c',
-      '#a8d4a9',
-      '#ffd479',
-      '#78aad6',
-      '#d7acd6',
-      '#76d4d6',
-      '#ffffff'
-    ]
+    colors: {
+      black: '#000000',
+      red: '#f78d8c',
+      green: '#a8d4a9',
+      yellow: '#ffd479',
+      blue: '#78aad6',
+      magenta: '#d7acd6',
+      cyan: '#76d4d6',
+      white: '#ffffff',
+      lightBlack: '#000000',
+      lightRed: '#f78d8c',
+      lightGreen: '#a8d4a9',
+      lightYellow: '#ffd479',
+      lightBlue: '#78aad6',
+      lightMagenta: '#d7acd6',
+      lightCyan: '#76d4d6',
+      lightWhite: '#ffffff'
+    }
   },
 
   plugins: [
-    'hyper-simple-vibrancy',
-    'hyper-statusline',
-    'hyperlinks'
+    'hyper-simple-vibrancy'
   ]
 }
